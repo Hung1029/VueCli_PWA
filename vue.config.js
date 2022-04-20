dbf8ae1b28430bcf9be69a7842365fa27df177e8
Hung1029 <psi94133@gmail.com>
2022-04-21 04:53:41 +0800

    Initial commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,34 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/eric-project/'
+    : '/'
+}, {
+
+  transpileDependencies: true,
+  pwa: {
+    name: 'pwa',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+
+    // 如果想要自己寫 service-worker.js
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'src/service-worker.js',
+      // ...other Workbox options...
+    },
+
+    // 如果想要讓他自己生產
+    workboxPluginMode: 'GenerateSW',
+    workboxOptions: {
+      // 不等待 terminated 直接取代舊的 service-worker
+      // 解決無法自動更新的問題
+      skipWaiting: true
+    }
+  }
 })
